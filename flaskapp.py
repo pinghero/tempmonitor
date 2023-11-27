98e49178d2d6d990e1a32c0c1f999ffea6381cb3
pinghero <lexor.fraud97@gmail.com>
2023-11-27 10:51:08 +0100

    Changed file structure of funcs

--- flaskapp.py
+++ flaskapp.py
@@ -7,6 +7,7 @@ from route_functions import *
 from database.db_models import *
 from flask_httpauth import HTTPBasicAuth
 from hmac import compare_digest
+from get_all_measurment_data import get_all_measurement_data
 import random
 
 app = Flask(__name__,template_folder='/home/pinghero/tempmonitor/templates/')
@@ -19,53 +20,39 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://pinghero:Cro0asan@local
 
 db.init_app(app)
 
-def get_all_measurement_data():
-    measurements = measurments.query.all()
-
-    data = []
-
-    for measurement in measurements:
-        timestamp = measurement.created_on.strftime("%Y-%m-%d %H:%M:%S")  # Convert datetime to string
-        data.append({
-            'location': measurement.location,
-            'temperature': float(measurement.temperature),
-            'humidity': float(measurement.humidity),
-            'timestamp': timestamp
-        })
-
-    return data
-
+@auth.login_required
 @app.route("/", methods=['GET'])
 def show_measurements():
-    # Use data from the database
-    all_measurement_data = get_all_measurement_data()
-
-    # Extract data for JavaScript
-    labels = sorted(list(set(measurement['timestamp'] for measurement in all_measurement_data)))
-    datasets = []
-
-    for location in set(measurement['location'] for measurement in all_measurement_data):
-        temperature_values = [measurement['temperature'] for measurement in all_measurement_data if measurement['location'] == location]
-        datasets.append({
-            'label': location,
-            'data': temperature_values,
-            'fill': False,
-            'borderColor': getRandomColor(),
-            'lineTension': 0.1
-        })
-
-    # Convert data to JSON format
-    data_json = {
-        'labels': labels,
-        'datasets': datasets
-    }
-
-    return render_template('measurements.html', data_json=json.dumps(data_json), temps=all_measurement_data)
-
-# Function to generate a random color
-def getRandomColor():
-    import random
-    return "#{:06x}".format(random.randint(0, 0xFFFFFF))
+    return show_measurements()
+#     # Use data from the database
+#     all_measurement_data = get_all_measurement_data()
+#
+#     # Extract data for JavaScript
+#     labels = sorted(list(set(measurement['timestamp'] for measurement in all_measurement_data)))
+#     datasets = []
+#
+#     for location in set(measurement['location'] for measurement in all_measurement_data):
+#         temperature_values = [measurement['temperature'] for measurement in all_measurement_data if measurement['location'] == location]
+#         datasets.append({
+#             'label': location,
+#             'data': temperature_values,
+#             'fill': False,
+#             'borderColor': getRandomColor(),
+#             'lineTension': 0.1
+#         })
+#
+#     # Convert data to JSON format
+#     data_json = {
+#         'labels': labels,
+#         'datasets': datasets
+#     }
+#
+#     return render_template('measurements.html', data_json=json.dumps(data_json), temps=all_measurement_data)
+#
+# # Function to generate a random color
+# def getRandomColor():
+#     import random
+#     return "#{:06x}".format(random.randint(0, 0xFFFFFF))
 
 @auth.verify_password
 def verify_password(username, password):
@@ -76,12 +63,7 @@ def verify_password(username, password):
             print("Pass is right")
             return username
     return False
-    
-#@app.route("/", methods=['GET'])
-#@auth.login_required
-#def index():
-#    return show_measurments(request=request)
- 
+
 @app.route("/add", methods=['POST'])
 @auth.login_required
 def add():
---
+++ show_measurments.py
@@ -0,0 +1,30 @@
+
+from flask import render_template
+def getRandomColor():
+    import random
+    return "#{:06x}".format(random.randint(0, 0xFFFFFF))
+def show_measurements():
+    # Use data from the database
+    all_measurement_data = get_all_measurement_data()
+
+    # Extract data for JavaScript
+    labels = sorted(list(set(measurement['timestamp'] for measurement in all_measurement_data)))
+    datasets = []
+
+    for location in set(measurement['location'] for measurement in all_measurement_data):
+        temperature_values = [measurement['temperature'] for measurement in all_measurement_data if measurement['location'] == location]
+        datasets.append({
+            'label': location,
+            'data': temperature_values,
+            'fill': False,
+            'borderColor': getRandomColor(),
+            'lineTension': 0.1
+        })
+
+    # Convert data to JSON format
+    data_json = {
+        'labels': labels,
+        'datasets': datasets
+    }
+
+    return render_template('measurements.html', data_json=json.dumps(data_json), temps=all_measurement_data)
