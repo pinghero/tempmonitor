06a66e92650892f57f96d8b7b14b958c5b085456
pinghero <lexor.fraud97@gmail.com>
2023-12-11 12:43:57 +0100

    Tests graph conversion method

--- show_measurments.py
+++ show_measurments.py
@@ -39,36 +39,11 @@ def show_measurements():
         })
 
     # Convert data to JSON format
+    print(labels)
+    print('-------------------------------------------------')
+    print(datasets)
     data_json = {
         'labels': labels,
         'datasets': datasets
     }
-    print(labels)
-    print('----------------------------')
-    print(datasets)
     return render_template('measurements.html', data_json=json.dumps(data_json), temps=all_measurement_data)
-    ####
-    # # Use data from the database
-    # all_measurement_data = get_all_measurement_data()
-    #
-    # # Extract data for JavaScript
-    # labels = sorted(list(set(measurement['timestamp'][:-9] for measurement in all_measurement_data)))
-    # datasets = []
-    #
-    # for location in set(measurement['location'] for measurement in all_measurement_data):
-    #     temperature_values = [measurement['temperature'] for measurement in all_measurement_data if measurement['location'] == location]
-    #     datasets.append({
-    #         'label': location,
-    #         'data': temperature_values,
-    #         'fill': False,
-    #         'borderColor': getRandomColor(),
-    #         'lineTension': 0.1
-    #     })
-    #
-    # # Convert data to JSON format
-    # data_json = {
-    #     'labels': labels,
-    #     'datasets': datasets
-    # }
-    #
-    # return render_template('measurements.html', data_json=json.dumps(data_json), temps=all_measurement_data)
