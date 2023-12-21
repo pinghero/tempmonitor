552384ede4a04715d6e7dfad7547095788c5ca52
Alex R <lexor.fraud97@gmail.com>
2023-12-21 15:10:51 +0100

    AJAX call tests

--- static/js/filter_data.js
+++ static/js/filter_data.js
@@ -1,60 +1,59 @@
 function applyFilters() {
-    var tempFrom = parseFloat(document.getElementById('tempFrom').value);
-    var tempTo = parseFloat(document.getElementById('tempTo').value);
-    var humidityFrom = parseFloat(document.getElementById('humidityFrom').value);
-    var humidityTo = parseFloat(document.getElementById('humidityTo').value);
-    var dateFrom = parseFloat(document.getElementById('dateFrom').value);
-    var dateTo = parseFloat(document.getElementById('dateTo').value);
-    var location = parseFloat(document.getElementById('location').value);
+    var tempFrom = parseFloat($('#tempFrom').val());
+    var tempTo = parseFloat($('#tempTo').val());
+    var humidityFrom = parseFloat($('#humidityFrom').val());
+    var humidityTo = parseFloat($('#humidityTo').val());
+    var dateFrom = parseFloat($('#dateFrom').val());
+    var dateTo = parseFloat($('#dateTo').val());
+    var location = parseFloat($('#location').val());
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/update_data", true);
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            // Update the table with the received data
-            var newData = JSON.parse(xhr.responseText);
-            updateTable(newData);
+    $.ajax({
+        url: "/update_data",
+        type: "POST",
+        contentType: "application/json;charset=UTF-8",
+        data: JSON.stringify({
+            tempFrom: tempFrom,
+            tempTo: tempTo,
+            humidityFrom: humidityFrom,
+            humidityTo: humidityTo,
+            dateFrom: dateFrom,
+            dateTo: dateTo,
+            location: location
+        }),
+        success: function (data) {
+            updateTable(data);
+        },
+        error: function (xhr, status, error) {
+            console.error("Error:", error);
         }
-    };
-
-    xhr.send(JSON.stringify({
-        tempFrom: tempFrom,
-        tempTo: tempTo,
-        humidityFrom: humidityFrom,
-        humidityTo: humidityTo,
-        dateFrom: dateFrom,
-        dateTo: dateTo,
-        location: location
-    }));
+    });
 }
 
 function resetFilters() {
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "/get_original_data", true);
-
-xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-        // Update the table with the received original data
-        var originalData = JSON.parse(xhr.responseText);
-        updateTable(originalData);
-    }
-};
-
-xhr.send();
+    $.ajax({
+        url: "/get_original_data",
+        type: "GET",
+        success: function (data) {
+            updateTable(data);
+        },
+        error: function (xhr, status, error) {
+            console.error("Error:", error);
+        }
+    });
 }
 
 function updateTable(data) {
-    var tableBody = document.querySelector('#myTable tbody');
-    tableBody.innerHTML = '';
+    var tableBody = $('#myTable tbody');
+    tableBody.empty();  // Clear the table body
 
-    // data.forEach(function (temp) {
-    //     var row = tableBody.insertRow();
-    //     window.alert(temp)
-    //     row.insertCell(0).textContent = temp.timestamp;
-    //     row.insertCell(1).textContent = temp.location;
-    //     row.insertCell(2).textContent = temp.temperature;
-    //     row.insertCell(3).textContent = temp.humidity;
-    // });
+    $.each(data, function (index, temp) {
+        var row = tableBody[0].insertRow();  // Access the raw DOM element using [0]
+
+        // Assuming 'timestamp', 'location', 'temperature', and 'humidity' are keys in your data
+        row.insertCell(0).textContent = temp.timestamp;
+        row.insertCell(1).textContent = temp.location;
+        row.insertCell(2).textContent = temp.temperature;
+        row.insertCell(3).textContent = temp.humidity;
+    });
 }
+
